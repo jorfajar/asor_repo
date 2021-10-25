bc109b1faffec2f1a557701733b857a93591ce94
jorfajar <93117498+jorfajar@users.noreply.github.com>
2021-10-25 09:55:41 +0200

    Update ejercicio2.cc

--- practica2.1/ejercicio2.cc
+++ practica2.1/ejercicio2.cc
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
 
+int main(){
+  char *s;
+  if(setuid(0) == -1){
+    printf("ERROR(%d): %s\n", errno, strerror(errno));
+  }
+  return 1;
+}
+
+/*
+  El código de error generado es:
+  ERROR(1): Operation not permitted
+*/
