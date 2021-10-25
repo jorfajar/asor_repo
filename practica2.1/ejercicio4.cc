e733ea107e0bd2ff184f18bea7cc3d2d6c974795
jorfajar <93117498+jorfajar@users.noreply.github.com>
2021-10-25 10:39:07 +0200

    Create ejercicio4.cc

---
+++ practica2.1/ejercicio4.cc
@@ -0,0 +1,6 @@
+/*
+He usado los comando man uname y uname --all. La salida de este último ha sido:
+
+Linux localhost.localdomain 3.19.9-862.11.6.el7.x86_64 #1 SMP Tye Aug 14 21:49:04 
+UTC 2018 x86_64 x86_64 x86_64 GNU/Linux
+*/
