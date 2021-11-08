0a4e064691a002d637b7575ce0a76306d99588b8
jorfajar <93117498+jorfajar@users.noreply.github.com>
2021-11-08 09:17:33 +0100

    Update ejercicio1.cc

--- practica2.2/ejercicio1.cc
+++ practica2.2/ejercicio1.cc
@@ -1,1 +1,11 @@
+man ls
 
+-a, -all: do not ignore entries starting with .
+-d, --directory: list directories themselves, not their contents
+-h, --human-readable: with -l, print sizes in human deadable format (e.g., 1K 234M 2G)
+-i, --inode: print the index number of each file
+-F, --classify: append indicator (one of */=>@|) to entries
+-l: use a long listing format
+-R, --recursive: list subdirectories recursively
+-1: list one file per line
+--color[=WHEN]: colorize the output; WHEN can be 'never', 'auto', or 'always' (the deault)
