15ca54f9f7470ac8e7b239fb454907681cec794f
jorfajar <93117498+jorfajar@users.noreply.github.com>
2021-11-26 09:25:11 +0100

    Update ejercicio2.cc

--- practica2.3/ejercicio2.cc
+++ practica2.3/ejercicio2.cc
@@ -8,7 +8,7 @@ int main(int argc, char **argv){
     return -1;
   }
   
-  int pid = atoi(argv[1]);
+  int pid = atoi(argv[0]);
   int scheduler = sched_getscheduler(pid);
   
   switch (scheduler){
