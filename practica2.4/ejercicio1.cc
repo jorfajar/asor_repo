4bee0baf9decc6889a1299e099892769cd0703b1
jorfajar <93117498+jorfajar@users.noreply.github.com>
2021-11-29 09:22:10 +0100

    Create ejercicio1.cc

---
+++ practica2.4/ejercicio1.cc
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define PIPE_W 1
+#define PIPE_R 0
+
+int main(int argc, char** argv){
+  if(argc < 2) printf("ERROR: introduce los comandos\n");
+  
+  int fd[2];
+  int pie = pipe(fd);
+  
+  switch(fork()){
+    case -1:
+      perror("ERROR: al hacer el fork");
+      return 1;
+      break;
+    case 0:
+      //HIJO
+      dup(fd[PIPE_R], 0);
+      close(fd[PIPE_W]);
+      close(fd[PIPE_R]);
+      execlp(argv[3], argv[3], argv[4]);
+      break;
+    default:
+      //PADRE
+      dup(fd[PIPE_W], 1);
+      close(fd[PIPE_W]);
+      close(fd[PIPE_R]);
+      execlp(argv[1], argv[1], argc[2]);
+      break;
+  }
+  
+  return 0;
+}
