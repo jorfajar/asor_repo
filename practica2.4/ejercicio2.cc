6328526067b9cfcb663d51e5bf713a95f3ee848e
jorfajar <93117498+jorfajar@users.noreply.github.com>
2021-11-29 10:04:24 +0100

    Update ejercicio2.cc

--- practica2.4/ejercicio2.cc
+++ practica2.4/ejercicio2.cc
@@ -1,16 +1,23 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
+#include <stdlib.h>
 
-#define PIPE_W 1
-#define PIPE_R 0
-
-int main(){
+int main(int argc, char** argv){
   int p_h[2];
   int h_p[2];
+  int fin = 0, m = 0;
   
-  pipe(p_h);
-  pipe(h_p);
+  if(pipe(p_h) == -1){
+    perror("pipe");
+    return -1;
+  }
+  
+  if(pipe(h_p) == -1){
+    perror("pipe");
+    return -1;
+  }
   
   switch(fork()){
     case -1:
@@ -19,16 +26,42 @@ int main(){
       break;
     case 0:
       close(p_h[1]);
-      close(h_p[1]);
+      close(h_p[0]);
       
-      for (size_t i = 0; i < 10; ++i){
-        ssize_t bytes = read(p_h[0], mensajePadre, 255);
-        mensajePadre[bytes] = '\0';
-        printf("[HIJO] Recibido %s\n", mensajePadre);
+      while(!fin){
+        int rc = read(p_h[0], buffer, 256);
+        buffer[rc] = '\0';
+        printf("Mensaje recibido: %s\n", buffer);
         sleep(1);
-        if(i == 9) flag = 'q';
-        write(h_p[1], &flag, 1);
+        if(++m == 10){
+          write(h_p[1], "q", 1);
+          fin = 1;
+        }else{
+          write(h_p1[1], "l", 1);
+        }
       }
+      close(p_h[0]);
+      close(h_p[1]);
+      return 0;
       break;
     default:
-      close(
+      close(p_h[0]);
+      close(h_p[1]);
+      
+      while(!fin){
+        printf("? ");
+        scanf("%s", buffer);
+        write(p_h[1], buffer, strlen(buffer)+1);
+        read(h_p[0], buffer, 1);
+        if(buffer[0] == 'q'){
+          fin = 1;
+        }
+      }
+      close(p_h[1]);
+      close(h_p[0]);
+      return 0;
+      break;
+  }
+  return 0;
+}
+        
